123a1df39bd582ee033245d995251fab8b43c2ee
Olga Larinova <olga.larinova@onlyoffice.com>
2022-05-30 15:48:28 +0300

    add

---
+++ config.php
@@ -0,0 +1,42 @@
+<?php
+$QUERYS = [
+    1 =>   "SELECT last_name as 'Фамилия', first_name as 'Имя', middle_name as 'Отчество', user_position.created_at as 'Дата найма', TIMESTAMPDIFF(MONTH, DATE(user_position.created_at), CURDATE() ) as 'Отработано месяцев'
+            FROM user, user_position 
+            WHERE user.id=user_position.user_id
+            AND TIMESTAMPDIFF(MONTH, DATE(user_position.created_at), CURDATE() ) < 3
+            ORDER BY last_name",
+
+    2 => "  SELECT last_name as 'Фамилия', first_name as 'Имя', middle_name as 'Отчество', description as 'Причина увольнения'
+            FROM user, user_dismission, dismission_reason
+            WHERE user.id = user_dismission.user_id AND user_dismission.reason_id=dismission_reason.id",
+
+    3 => "  SELECT last_users.name as 'Отделение', last_users.leader as 'Начальник',last_name as 'Фамилия', first_name as 'Имя', last_users.date as 'Дата найма'
+            FROM
+                (SELECT department.name as name, user.last_name as leader, MAX(user_position.user_id) as user_id, MAX(user_position.created_at) as date
+                FROM user_position
+                JOIN department
+                    ON department.id=user_position.department_id
+                JOIN user
+                    ON user.id=department.leader_id
+                GROUP BY department.id) as last_users
+            JOIN user
+            ON user.id = last_users.user_id ",
+
+    4 => "SELECT * FROM user"
+];
+
+// строк в таблице
+$LIMIT = 3;
+// текущая страница
+$PAGE = 1;
+// кнопок в пагинаторе
+$LINKS = 5;
+// номер запроса
+$QUERY = 1;
+
+// подключение к бд
+$HOST = 'mysql';
+$USER = 'admin';
+$PASS = 'test';
+$DB = 'test';
+$PORT = 3306;
